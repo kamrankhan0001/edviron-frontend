290838a1b704523c90c6b9a7f08c9b28d85e2895
kamrankhan0001 <kamranbgp15@gmail.com>
2024-12-31 20:39:59 +0530

    edviron-frontend

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import TransactionBySchool from "./pages/TransactionBySchool";
 import CheckStatus from "./pages/CheckStatus";
 //import Pagination from './components/Pagination';
 
-
-
 const App = () => {
     return (
         <Router>
@@ -33,29 +31,5 @@ export default App;
 
 
 
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Navbar from './components/Navbar';
-// import TransactionsOverview from './pages/Dashboard';
-// import SchoolTransactions from './pages/TransactionBySchool';
-// import TransactionStatusCheck from './pages/CheckStatus';
 
-// function App() {
-//   return (
-//     <Router>
-//       <div className="min-h-screen bg-gray-100">
-//         <Navbar />
-//         <div className="container mx-auto px-4 py-8">
-//           <Routes>
-//             <Route exact path="/" component={TransactionsOverview} />
-//             <Route path="/school-transactions" component={SchoolTransactions} />
-//             <Route path="/status-check" component={TransactionStatusCheck} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
